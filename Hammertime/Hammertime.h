f922e66a11fb595dd17ca79d843262ccf7e7574e
Chris Jones <cmsj@tenshu.net>
2021-10-20 15:53:53 +0100

    Initial Commit

---
+++ Hammertime/Hammertime.h
@@ -0,0 +1,18 @@
+//
+//  Hammertime.h
+//  Hammertime
+//
+//  Created by Chris Jones on 20/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Hammertime.
+FOUNDATION_EXPORT double HammertimeVersionNumber;
+
+//! Project version string for Hammertime.
+FOUNDATION_EXPORT const unsigned char HammertimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hammertime/PublicHeader.h>
+
+
